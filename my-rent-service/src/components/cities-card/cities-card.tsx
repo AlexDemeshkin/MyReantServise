2560f23ebd9ca691e9933d59e78b6257ee8a90e8
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2025-02-28 02:10:54 +0300

    Update cities-card.tsx

--- my-rent-service/src/components/cities-card/cities-card.tsx
+++ my-rent-service/src/components/cities-card/cities-card.tsx
@@ -1,20 +1,38 @@
-import { JSX } from "react"
+import { JSX, useState } from "react"
 import { Link } from "react-router-dom";
-function CitiesCard(): JSX.Element {
+import { AppRoute } from "../../const";
+
+type CitiesCardProps = {
+  id: string;
+  title: string;
+  type : string;
+  price : number;
+  isPremium: boolean;
+  previewImage : string;
+  rating: number;
+}
+
+
+
+
+function CitiesCard({id,title,type,price,isPremium,previewImage,rating} : CitiesCardProps): JSX.Element {
+  const [, setOfferId] =useState('')
     return(
-        <article className="cities__card place-card">
-        <div className="place-card__mark">
-          <span>Premium</span>
-        </div>
+        <article className="cities__card place-card" onMouseOver={() => setOfferId(id)} onMouseOut={() => setOfferId('')}>
+              {isPremium ? (
+                <div className="place-card__mark">
+                  <span>Premium</span>
+                </div>) : null
+                } 
         <div className="cities__image-wrapper place-card__image-wrapper">
-          <Link to="/offer">
-            <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+          <Link to={`${AppRoute.Offer}/${id}`}>
+            <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image"/>
           </Link>
         </div>
         <div className="place-card__info">
           <div className="place-card__price-wrapper">
             <div className="place-card__price">
-              <b className="place-card__price-value">&euro;120</b>
+              <b className="place-card__price-value">&euro;{price}</b>
               <span className="place-card__price-text">&#47;&nbsp;night</span>
             </div>
             <button className="place-card__bookmark-button button" type="button">
@@ -26,14 +44,14 @@ function CitiesCard(): JSX.Element {
           </div>
           <div className="place-card__rating rating">
             <div className="place-card__stars rating__stars">
-              <span style={{width: "80%"}}></span>
+              <span style={{width:`${(rating / 5) * 100}%` }}></span>
               <span className="visually-hidden">Rating</span>
             </div>
           </div>
           <h2 className="place-card__name">
-            <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+            <a href="#">{title}</a>
           </h2>
-          <p className="place-card__type">Apartment</p>
+          <p className="place-card__type">{type}</p>
         </div>
       </article>
     )
