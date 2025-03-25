a7c682c9badb60ec3aa0ff398707f494c3b51f13
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2025-03-25 17:49:11 +0300

    Update favorite-card-list.tsx

--- my-rent-service/src/components/favorite-card-list/favorite-card-list.tsx
+++ my-rent-service/src/components/favorite-card-list/favorite-card-list.tsx
@@ -1,27 +1,43 @@
 import { JSX } from "react";
-import { OfferList } from "../../types/offer";
+import { OffersList } from "../../types/offer";
 import FavoritesCard from "../favorite-card/favorite-card";
 
 type FavotitesCardListProps = {
-    offersList : OfferList[];
+    offersList : OffersList[];
+    city: string;
 };
 
-
-function FavoritesCardList({offersList} : FavotitesCardListProps ): JSX.Element {
-    return(
-        <li className="favorites__locations-items">
+function FavoritesCardList({offersList,city} : FavotitesCardListProps ): JSX.Element {
+  const favoriteOffers = offersList.filter(
+    (item) => item.isFavorite && item.city.name === city);
+      return(      
+        <li className="favorites__locations-items" >
                 <div className="favorites__locations locations locations--current">
                   <div className="locations__item">
                     <a className="locations__item-link" href="#">
-                      <span>City</span>
+                      <span>{city}</span>
                     </a>
                   </div>
                 </div>
                 <div className="favorites__places">
-                {Array.from(offersList, (item) =>
-                  <FavoritesCard key={ item.id } id = { item.id } title = { item.title }  type = { item.type } price = { item.price } 
-                            previewImage = { item.previewImage } isPremium = {item.isPremium} rating = { item.rating } />)}
-
+                  {favoriteOffers.length > 0 ? (
+                    favoriteOffers.map((item) => (
+                      <FavoritesCard
+                        key={item.id}
+                        id={item.id}
+                        city={item.city.name}
+                        title={item.title}
+                        type={item.type}
+                        price={item.price}
+                        previewImage={item.previewImage}
+                        isPremium={item.isPremium}
+                        rating={item.rating}
+                        isFavorite={item.isFavorite}
+                      />
+                    ))
+                  ) : (
+                    <p>No favorites yet.</p>
+                  )}
                 </div>
               </li>
         );
