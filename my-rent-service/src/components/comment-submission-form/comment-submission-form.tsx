2f7972bf7206e37281d4c6a8f5efcdffd4a89e0c
AlexDemeshkin <163833861+AlexDemeshkin@users.noreply.github.com>
2025-03-30 06:37:36 +0300

    Update comment-submission-form.tsx

--- my-rent-service/src/components/comment-submission-form/comment-submission-form.tsx
+++ my-rent-service/src/components/comment-submission-form/comment-submission-form.tsx
@@ -1,55 +1,70 @@
-import { JSX } from "react";
+import { JSX, useState } from "react";
 
 function CommentSubmissionForm(): JSX.Element {
-    return(
-        <form className="reviews__form form" action="#" method="post">
-                    <label className="reviews__label form__label" htmlFor="review">Your review</label>
-                    <div className="reviews__rating-form form__rating">
-                      <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio"/>
-                      <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use href="#icon-star"></use>
-                        </svg>
-                      </label>
-  
-                      <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
-                      <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use href="#icon-star"></use>
-                        </svg>
-                      </label>
-  
-                      <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
-                      <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use href="#icon-star"></use>
-                        </svg>
-                      </label>
-  
-                      <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
-                      <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use href="#icon-star"></use>
-                        </svg>
-                      </label>
-  
-                      <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
-                      <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use href="#icon-star"></use>
-                        </svg>
-                      </label>
+    const [comment, setComment] = useState("");
+    const [rating, setRating] = useState(0);
+
+    const handleCommentChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setComment(evt.target.value);
+    };
+
+    const handleRatingChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        setRating(Number(evt.target.value));
+    };
+
+    const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+        evt.preventDefault();
+        console.log({ comment, rating });
+    };
+
+    return (
+        <form className="reviews__form form" action="#" method="post" onSubmit={handleSubmit}>
+            
+            <label className="reviews__label form__label"  htmlFor="review">Your review</label>
+            <div className="reviews__rating-form form__rating">
+                {[5, 4, 3, 2, 1].map((star) => (
+                    <div key={star}>
+                        <input 
+                            className="form__rating-input visually-hiddens"
+                            style={{ display: 'none' }}
+                            name="rating"
+                            value={star}
+                            id={`${star}-stars`}
+                            type="radio"
+                            checked={rating === star}
+                            onChange={handleRatingChange}
+                        />
+
+                        <label htmlFor={`${star}-stars`} className="reviews__rating-label form__rating-label">
+                            <svg className="form__star-image " width="37" height="33">
+                                <use xlinkHref="#icon-star"></use>
+                            </svg>
+                        </label>
                     </div>
-                    <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
-                    <div className="reviews__button-wrapper">
-                      <p className="reviews__help">
-                        To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                      </p>
-                      <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-                    </div>
-                  </form>
-        );
-    }
+                ))}
+            </div>
+            <textarea
+                className="reviews__textarea form__textarea"
+                id="review"
+                name="review"
+                placeholder="Tell how was your stay, what you like and what can be improved"
+                value={comment}
+                onChange={handleCommentChange}
+            ></textarea>
+            <div className="reviews__button-wrapper">
+                <p className="reviews__help">
+                    To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+                </p>
+                <button
+                    className="reviews__submit form__submit button"
+                    type="submit"
+                    disabled={comment.length < 50 || rating === 0}
+                >
+                    Submit
+                </button>
+            </div>
+        </form>
+    );
+}
 
 export default CommentSubmissionForm;
-    
